3240f443a4fec3b17b79fc851058659effd03eb5
sophiealt <48678791+sophiealt@users.noreply.github.com>
2019-04-09 18:01:01 +0300

    Delete getdata.php

--- getdata.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sql = mysqli_query($conn, 'SELECT `ID`, `name`, `email`, `country` FROM `users`');
-while ($result = mysqli_fetch_array($sql)) {
-    echo "<p>{$result['ID']}) {$result['name']} - {$result['email']} - {$result['country']} - <a href='?del={$result['ID']}'>Delete</a> - <a href='?red={$result['ID']}'>Change</a></p>";
-}
-
-
-?>
